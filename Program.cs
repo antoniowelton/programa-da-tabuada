2d356f4e70be00ae6d2f8e53756dd6877d708147
antoniowelton <antonioweltondev@gmail.com>
2025-11-25 20:22:05 -0300

    Atualizei os comentários

--- Program.cs
+++ Program.cs
@@ -6,16 +6,16 @@ class Program
     {
 
         
-        Console.WriteLine("\nDigite um número pra ver sua tabuada: \n");
-        int numero = Convert.ToInt32(Console.ReadLine());
-        Console.Write("\n");
+        Console.WriteLine("\nDigite um número pra ver sua tabuada: \n"); // Pede que o usuário informe um número pra ver sua tabuada
+        int numero = Convert.ToInt32(Console.ReadLine()); // Converte o número texto pra uma variável do tipo INTEIRO
+        Console.Write("\n"); // Escreve uma linha em branco
 
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i <= 10; i++) // Repete o multiplicador de 0 até 10 sempre somando + 1 a cada iteração
         {
-            Console.Write($"{numero} x {i} = {numero * i}\n");
+            Console.Write($"{numero} x {i} = {numero * i}\n"); // Escreve as linhas da tabuada
         }
 
-        Console.Write("\nFIM DA TABUADA!\n\n");
+        Console.Write("\nFIM DA TABUADA!\n\n"); // Escreve FIM DA TABUADA
 
         
     }
