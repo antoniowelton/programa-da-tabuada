ceeeda9ce97c57d3a5f5e7c49fa70ff2e31bc6ea
antoniowelton <antonioweltondev@gmail.com>
2025-11-25 20:14:59 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace programa_da_tabuada;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+        Console.WriteLine("\nDigite um número pra ver sua tabuada: \n");
+        int numero = Convert.ToInt32(Console.ReadLine());
+        Console.Write("\n");
+
+        for (int i = 0; i <= 10; i++)
+        {
+            Console.Write($"{numero} x {i} = {numero * i}\n");
+        }
+
+        Console.Write("\nFIM DA TABUADA!\n\n");
+
+        
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# programa-da-tabuada
+Programa em C# que calcula a tabuada de um número
